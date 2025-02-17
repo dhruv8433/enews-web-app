04da7f9bd9a487fc5387d7bd5cb55029810aa486
Soni Dhruv <dhruvrsoni04@gmail.com>
2025-02-17 17:44:15 +0530

    small card layout for swiper

---
+++ app/common/SmallCard.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { HeadlineProps } from '../types/headline.types';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import { imageUrl } from '../site/site.config';
+
+const SmallCard: React.FC<HeadlineProps> = ({ headline }) => {
+    return (
+        <div className="group relative shadow-md rounded-lg overflow-hidden transition-all duration-300 hover:shadow-xl">
+            {/* Image Container with Title Overlay */}
+            <div className="relative w-full h-60 overflow-hidden">
+                <img
+                    src={imageUrl + headline.multimedia[0]?.url}
+                    alt={headline.headline.main}
+                    className="absolute inset-0 w-full h-full object-cover transition-transform duration-300 group-hover:scale-110"
+                />
+                
+                {/* Title with Backdrop */}
+                <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black/80 via-black/40 to-transparent p-3">
+                    <h3 className="text-white text-lg font-semibold line-clamp-2 transition-all duration-300">
+                        {headline.headline.main}
+                    </h3>
+                </div>
+            </div>
+
+           
+        </div>
+    );
+};
+
+export default SmallCard;
