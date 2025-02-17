917a46b9baa02033f66eda1856d901671d477967
Soni Dhruv <dhruvrsoni04@gmail.com>
2025-02-17 12:55:50 +0530

    site global configurations

---
+++ app/site/site.config.ts
@@ -0,0 +1,48 @@
+/**
+ * Site configuration file
+ * 2025, All rights reserved
+ */
+
+import { Facebook, GitHub, Instagram, LinkedIn, Twitter } from '@mui/icons-material'
+import { Routes } from '../types/routes.types';
+import { navIcon } from '../types/navicon.types';
+
+// navigation routes for website
+const routes: Routes[] = [{
+    name: "Home",
+    url: "/"
+},
+{
+    name: "Popular",
+    url: '/popular'
+},
+{
+    name: "live",
+    url: "/live"
+}]
+
+//icons that used for header
+const navIcons: navIcon[] = [{
+    name: 'insta',
+    url: "https://www.instagram.com/dhruv_s_o_n_i_",
+    icon: Instagram
+}, {
+    name: "Twitter",
+    icon: Twitter,
+    url: "https://x.com/dhruv_s_o_n_i"
+}, {
+    name: "Github",
+    icon: GitHub,
+    url: "https://github.com/dhruv8433"
+}, {
+    name: "Facebook",
+    icon: Facebook,
+    url: "https://www.facebook.com/dhruv.soni.9809"
+}, {
+    name: "LinkedIn",
+    icon: LinkedIn,
+    url: "https://www.linkedin.com/in/dhruv-soni-6b2338230/"
+}
+];
+
+export { routes, navIcons };
