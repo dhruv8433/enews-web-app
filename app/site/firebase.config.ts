4b1295a6071db4a7fe965e96ca22aaae82c1feda
Soni Dhruv <dhruvrsoni04@gmail.com>
2025-02-19 10:37:03 +0530

    add firebase configuration and google auth

--- app/site/firebase.config.ts
+++ app/site/firebase.config.ts
@@ -6,10 +6,10 @@ const firebaseConfig = {
   apiKey: "AIzaSyAWIZbFJI_HXiP0bXx6nGlI8ipwA5cDB_0",
   authDomain: "news-app-e1567.firebaseapp.com",
   projectId: "news-app-e1567",
-  storageBucket: "news-app-e1567.appspot.com",
+  storageBucket: "news-app-e1567.firebasestorage.app",
   messagingSenderId: "348954268899",
   appId: "1:348954268899:web:d887b9d74a1dbbac5021d6",
-  measurementId: "G-V731YPM4Z9",
+  measurementId: "G-V731YPM4Z9"
 };
 
 // Initialize Firebase
@@ -18,15 +18,15 @@ const db = getFirestore(app);
 export const auth = getAuth(app);
 
 // Enable offline persistence
-enableIndexedDbPersistence(db)
-  .then(() => {
-    // Offline persistence enabled successfully
-    console.log("Offline support enabled");
-  })
-  .catch((error) => {
-    // Error handling if offline persistence fails
-    console.error("Error enabling offline support:", error);
-  });
+// enableIndexedDbPersistence(db)
+//   .then(() => {
+//     // Offline persistence enabled successfully
+//     console.log("Offline support enabled");
+//   })
+//   .catch((error) => {
+//     // Error handling if offline persistence fails
+//     console.error("Error enabling offline support:", error);
+//   });
 
 export { app, db };
 
