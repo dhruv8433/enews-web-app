5030cef2392b7f497afe56d0a0ed65011473b43a
Soni Dhruv <dhruvrsoni04@gmail.com>
2025-02-28 19:28:43 +0530

    update real time weater info

--- app/hooks/useWeather.ts
+++ app/hooks/useWeather.ts
@@ -1,18 +1,24 @@
 import { useEffect, useState } from "react";
 import { getWeatherInfo } from "../service/getWeatherInfo";
+import notifications from "../constants/notifications";
 
 export const useWeather = (location: string) => {
     const [weather, setWeather] = useState<any>(null);
     const [loading, setLoading] = useState<boolean>(true);
-    const [error, setError] = useState<string | null>(null);
+    const [error, setError] = useState<{ message: string; details?: string } | null>(null);
 
-    async function getInfo(){
-        const response = await getWeatherInfo(location);
-        if(response.status === 200){
-            setWeather(response.data);
-            setLoading(false);
-        } else {
-            setError(response.statusText);
+    async function getInfo() {
+        try {
+            const response = await getWeatherInfo(location);
+            if (response.status === 200) {
+                setWeather(response.data);
+                setLoading(false);
+            }
+        } catch (error) {
+            setError({
+                message: notifications.error.weatherInfoFailed.message,
+                details: (error as Error).message || notifications.error.weatherInfoFailed.description,
+            });
             setLoading(false);
         }
     }
