a60a55cd4ed5a2899ebe509f392eded6fcc5fbe8
Soni Dhruv <dhruvrsoni04@gmail.com>
2025-02-28 19:29:10 +0530

    update comment model

--- app/common/CommentModel.tsx
+++ app/common/CommentModel.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import useComments from "../hooks/useComments";
-import toast from "react-hot-toast";
 import notifications from "../constants/notifications";
+import toast from "react-hot-toast";
 
 const CommentModal = ({ articleId, onClose }: { articleId: string; onClose: () => void }) => {
     const [comment, setComment] = useState("");
@@ -31,7 +31,7 @@ const CommentModal = ({ articleId, onClose }: { articleId: string; onClose: () =
                 <div className="flex justify-end mt-4">
                     <button className="bg-gray-400 px-4 py-2 rounded mr-2" onClick={onClose}>Cancel</button>
                     <button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={handleSubmit}>
-                        Add Comment
+                        Comment
                     </button>
                 </div>
             </div>
