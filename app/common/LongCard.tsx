b36586908c7ed3ec8fdee807896447ddef3fa0fb
Soni Dhruv <dhruvrsoni04@gmail.com>
2025-02-28 17:33:16 +0530

    update long card design

--- app/common/LongCard.tsx
+++ app/common/LongCard.tsx
@@ -9,7 +9,7 @@ import { handleShareArticle } from "../service/ShareArticleService";
 
 const LongCard: React.FC<HeadlineProps> = ({ headline }) => {
     return (
-        <div className="relative max-w-3xl h-[400px] bg-white shadow-lg rounded-xl overflow-hidden transition-transform duration-300 hover:shadow-2xl group hover:cursor-pointer">
+        <div className="relative max-w-3xl h-[500px] bg-white shadow-lg rounded-xl overflow-hidden transition-transform duration-300 hover:shadow-2xl group hover:cursor-pointer">
 
             {/* Like Button - Positioned in the top-right corner */}
             <div className="absolute top-3 right-3 bg-white p-1 rounded-full shadow-md z-10">
@@ -20,11 +20,11 @@ const LongCard: React.FC<HeadlineProps> = ({ headline }) => {
             <Link href={`/detail/${slugify(headline.abstract).toLowerCase()}`}>
                 <div className="" onClick={() => handleShareArticle(headline)}>
                     {/* Article Image */}
-                    <div className="relative w-full h-52 overflow-hidden">
+                    <div className="w-full h-[350px] overflow-hidden">
                         <img
                             src={headline.multimedia[0]?.url ? imageUrl + headline.multimedia[0].url : "/placeholder.jpg"}
                             alt={headline.headline.main}
-                            className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
+                            className="w-full h-full object-fill transition-transform duration-300 group-hover:scale-105"
                         />
                         <div className="absolute top-4 left-4 bg-red-500 text-white text-xs font-semibold px-3 py-1 rounded-md shadow-md">
                             Featured
@@ -34,8 +34,8 @@ const LongCard: React.FC<HeadlineProps> = ({ headline }) => {
                     {/* Content Section */}
                     <div className="p-6 flex flex-col space-y-4">
                         {/* Title */}
-                        <h2 className="text-2xl font-bold text-gray-900 group-hover:text-blue-900 transition-colors duration-300">
-                            {headline.headline.main.length > 60
+                        <h2 className="text-xl font-bold text-gray-900 group-hover:text-blue-900 transition-colors duration-300">
+                            {headline.headline.main.length > 20
                                 ? `${headline.headline.main.substring(0, 60)}...`
                                 : headline.headline.main}
                         </h2>
