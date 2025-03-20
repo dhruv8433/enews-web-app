00d9aa1a1cfe79cc8808e09c4e3d72d4e1d39366
Soni Dhruv <dhruvrsoni04@gmail.com>
2025-03-20 20:56:51 +0530

    update and add theme in my heading 🎨

--- app/common/MyHeading.tsx
+++ app/common/MyHeading.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const MyHeading = ({ title }: { title: string }) => {
     return (
         <div className="custom-heading">
-            <div className="p-[5px] bg-blue-700  w-40 flex justify-center text-white">
+            <div style={{ background: `var(--primary)`, color: `var(--text)` }} className="p-[5px] w-40 flex justify-center text-white">
                 {title}
             </div>
         </div>
