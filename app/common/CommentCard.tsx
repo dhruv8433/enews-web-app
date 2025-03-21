1d10fcad1ae2adbf8f4ddaaa4ca89e77f30e3eef
Soni Dhruv <dhruvrsoni04@gmail.com>
2025-03-21 11:18:48 +0530

    update comment card and add theme 🎨

--- app/common/CommentCard.tsx
+++ app/common/CommentCard.tsx
@@ -3,21 +3,23 @@ import { motion } from "framer-motion";
 import { CommentCardProps } from "../types/comments.types";
 
 const CommentCard: React.FC<CommentCardProps> = ({ user, comment, timestamp }) => {
+  console.log(timestamp.toString());
   return (
     <motion.div
       initial={{ opacity: 0, y: 30 }}
       animate={{ opacity: 1, y: 0 }}
       whileHover={{ scale: 1.02 }}
       transition={{ duration: 0.4, ease: "easeInOut" }}
-      className="bg-white shadow-lg rounded-lg p-5 flex gap-4 border border-gray-200 hover:shadow-2xl transition-shadow duration-300 my-2"
+      style={{ backgroundColor: `var(--secondary)` }}
+      className=" shadow-lg rounded-lg p-5 flex gap-4 border border-gray-200 hover:shadow-2xl transition-shadow duration-300 my-2"
     >
       {/* Comment Content */}
       <div className="flex-1">
         <div className="flex justify-between items-center">
-          <h3 className="font-bold text-gray-900 text-lg">{user}</h3>
-          <span className="text-xs font-medium text-gray-500">{timestamp}</span>
+          <h3 className="font-bold text-lg" style={{color: `var(--text)`}}>{user}</h3>
+          <span className="text-xs font-medium" style={{color: `var(--text)`}}>{timestamp}</span>
         </div>
-        <p className="text-gray-700 mt-2 text-sm leading-relaxed">{comment}</p>
+        <p className="mt-2 text-sm leading-relaxed" style={{color: `var(--text)`}}>{comment}</p>
       </div>
     </motion.div>
   );
