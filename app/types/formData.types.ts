cd8f717e309544e295b78f27475c576153812191
Soni Dhruv <dhruvrsoni04@gmail.com>
2025-05-07 17:13:06 +0530

    add types of user data and update auth service

---
+++ app/types/formData.types.ts
@@ -0,0 +1,10 @@
+export type FormDataType = {
+    name?: string;
+    email: string;
+    password: string;
+    confirmPassword?: string;
+    phone_no?: string;
+    avatar?: File
+    fullName?: string;
+    avatar_url?: string;
+}
