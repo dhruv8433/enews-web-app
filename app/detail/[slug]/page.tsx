6a844d5a7eafef8b56c454fe31e1c188487e062c
Soni Dhruv <dhruvrsoni04@gmail.com>
2025-05-17 22:31:38 +0530

    resolve

--- app/detail/[slug]/page.tsx
+++ app/detail/[slug]/page.tsx
@@ -1,10 +1,6 @@
 "use client";
 
 import React from "react";
-import LargeContainer from "@/app/common/LargeContainer";
-import { Grid } from "@mui/material";
-import DetailedNews from "@/app/common/DetailedNews";
-import RelatedNews from "@/app/common/RelatedNews";
 import { useParams } from "next/navigation";
 
 const Page: React.FC = () => {
@@ -16,16 +12,7 @@ const Page: React.FC = () => {
   console.log("slug", slugString);
 
   return (
-    <LargeContainer>
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={8}>
-          <DetailedNews slug={slugString} />
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <RelatedNews slug={slugString} />
-        </Grid>
-      </Grid>
-    </LargeContainer>
+    <> Detailed Page addedd soon</>
   );
 };
 
