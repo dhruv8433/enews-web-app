9356788f6799eb060ae05e0a8463a2ed3320dda2
Soni Dhruv <dhruvrsoni04@gmail.com>
2025-05-18 12:42:38 +0530

    layout completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Toaster } from 'react-hot-toast'
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
 import GlobalAdvertise from "./components/headers/GlobalAdvertise";
+import { AuthProvider } from "./context/AuthContext";
 
 export default function RootLayout({
   children,
@@ -14,17 +15,17 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`antialiased`} suppressHydrationWarning>
-        <div className="">
-          <GlobalAdvertise />
-          <Navbar />
-          <div className="container">
-
-            {children}
+        <AuthProvider>
+          <div className="">
+            <GlobalAdvertise />
+            <Navbar />
+            <div className="container">
+              {children}
+            </div>
+            <Toaster position="top-center" reverseOrder={false} />
+            <Footer />
           </div>
-          <Toaster position="top-center" reverseOrder={false} />
-
-          <Footer />
-        </div>
+        </AuthProvider>
       </body>
     </html>
   );
